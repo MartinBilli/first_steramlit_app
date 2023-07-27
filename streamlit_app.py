929d5a7beae016e39a0275a3105faa308c56fca9
MartinBilli <128307349+MartinBilli@users.noreply.github.com>
2023-07-27 15:23:49 +0200

    Update streamlit_app.py
    corrected name definition

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 
 streamlit.header('Breakfast menu')
-sreamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Smoothie')
